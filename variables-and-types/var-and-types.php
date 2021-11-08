0583200dcda2cfdbc0c78689d5aa518a8220f748
Severo-Yannick <severoyannick@gmail.com>
2021-11-08 18:22:30 +0100

    exercise var-and-types

---
+++ variables-and-types/var-and-types.php
@@ -0,0 +1,15 @@
+<?php
+// Partie 1: ajoutez les variables name et age.
+$name = "Jacques";
+$age = 20;
+echo "Salut $name. Vous avez $age ans.\n";
+
+// Part 2: faites la somme des variables x et y
+// et mettez le résultat dans la variable sum.
+
+$x = 195793;
+$y = 256836;
+$sum = $x + $y;
+
+echo "La somme de $x et $y est $sum."
+?>
