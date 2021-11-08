10f5ce13e262c8e77c82d7d1929e0cfe04fa9e9e
Severo-Yannick <severoyannick@gmail.com>
2021-11-08 18:41:19 +0100

    exercise simple-arrays

---
+++ simple-arrays/simple-arrays.php
@@ -0,0 +1,14 @@
+<?php
+
+// TODO: ajouter le tableau even_numbers ici
+$even_numbers = [2,4,6,8,10];
+
+// TODO: grouppez les noms masculins et féminins dans un seul tableau
+$male_names = ["Jacques", "Éric", "Jean"];
+$female_names = ["Jessica", "Beth", "Sandra"];
+$names = array_merge($male_names, $female_names);
+
+print_r($even_numbers);
+print_r($names);
+
+?>
